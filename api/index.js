e28e17c4c9202860cc516e3ac7e47ff130f0cc03
biaska <biaska70@gmail.com>
2024-01-30 21:30:17 -0800

    changed route message

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('A message from CS361')
 })
 
 const server = app.listen(port, () => {
