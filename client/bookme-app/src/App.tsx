d73bec45b54c2150666509b5ff9eb72ee8d5f0c6
biaska <biaska70@gmail.com>
2024-01-31 23:46:18 -0800

    Created react-ts and navbar

---
+++ client/bookme-app/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <div className="card">
+        <h1>Hello World.</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ client/bookme-app/src/Navbar.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Navbar() {
+
+    return(
+        <>
+            <nav>
+                <a href="google.com">Google</a>
+            </nav>
+        </>
+    )
+}
