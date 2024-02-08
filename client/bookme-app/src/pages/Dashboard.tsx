c674c5fcfd2d7646e90a720f03d04d714787b003
Kyle <biaska70@gmail.com>
2024-02-07 19:02:13 -0800

    working user data from auth

--- client/bookme-app/src/pages/Dashboard.tsx
+++ client/bookme-app/src/pages/Dashboard.tsx
@@ -1,5 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useAPI } from "../hooks/useApi";
+import { useAuthUserData } from "../hooks/useAuthUserData";
+import { useEffect, useState } from "react";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
@@ -13,18 +15,29 @@ const LogoutButton = () => {
 
 
 export default function Dashboard() {
+  const [userData, setUserData] = useState<any>()
+  const [userDetails, setUserDetails] = useState<any>()
+  const { user, userMetaData } = useAuthUserData();
   const callAPI = useAPI()
 
-    const resetDatabase = async () => {
+    const makeAPICall = async () => {
       await callAPI.get('/');
       console.log(callAPI.data)
     }
+  useEffect(() => {
+    setUserData(userMetaData);
+    setUserDetails(user);
+
+  }, [])
 
     return(
         <>
-            <button onClick={()=>resetDatabase()}>Load/Reset Database</button>
+            <button onClick={()=>makeAPICall()}>Make API call</button>
             <LogoutButton />
             <h1>Dashboard</h1>
+            {userData ? <p>UserMetaData loaded: {userData} {userDetails?.name}</p> :
+              <div>loading...</div>
+            }
         </>
     )
 }
